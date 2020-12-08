f2220492e1ce5db6ee8a403dd454f69853e9cf02
mila89 <doycheva_m@abv.bg>
2020-12-08 22:18:43 +0200

    Add files via upload

---
+++ Conditions/GodzillaKong/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GodzillaKong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double filmBudjet = double.Parse(Console.ReadLine());
+            int statists = int.Parse(Console.ReadLine());
+            double clothPrice = double.Parse(Console.ReadLine());
+            double decorPrice = filmBudjet * 10 / 100;
+            double clothBudjet = 0;
+            if (statists <= 150)
+                clothBudjet = statists * clothPrice;
+            else
+                clothBudjet = statists * clothPrice * 0.9;
+            double clothDecorAmount = clothBudjet+decorPrice;
+            if (clothDecorAmount > filmBudjet)
+            {
+                double neededMoney = clothDecorAmount - filmBudjet;
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {neededMoney,2:F} leva more.");
+            }
+            else 
+            { 
+                double neededMoney = filmBudjet - clothDecorAmount;
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {neededMoney,2:F} leva left.");
+            }
+        }
+    }
+}
