92334767cab93700943afae336ffac56c045993d
mila89 <doycheva_m@abv.bg>
2020-12-08 22:24:10 +0200

    Add files via upload

---
+++ Conditions/SumSeconds/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SumSeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstTime = int.Parse(Console.ReadLine());
+            int secondTime = int.Parse(Console.ReadLine());
+            int thirdTime = int.Parse(Console.ReadLine());
+            int sumTime = firstTime + secondTime + thirdTime;
+            int sumSeconds = sumTime % 60;
+            int sumMinutes = (sumTime / 60);
+
+            if (sumSeconds<10)
+                Console.WriteLine($"{ sumMinutes}:0{ sumSeconds}");
+            else Console.WriteLine($"{ sumMinutes}:{ sumSeconds}");
+        }
+    }
+}
