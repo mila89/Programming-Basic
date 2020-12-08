be80ab04b5f8849c2dc489f5a4ae3890eed9dd73
mila89 <doycheva_m@abv.bg>
2020-12-08 22:23:23 +0200

    Add files via upload

---
+++ Conditions/SpeedInfo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SpeedInfo
+{
+    class Program
+    {
+        static void Main()
+        {
+            double speedNum = double.Parse(Console.ReadLine());
+            string infoSpeed = "";
+            if (speedNum <= 10)
+                infoSpeed = "slow";
+            else if (speedNum <= 50)
+                infoSpeed = "average";
+            else if (speedNum <= 150)
+                infoSpeed = "fast";
+            else if (speedNum <= 1000)
+                infoSpeed = "ultra fast";
+            else if (speedNum > 1000)
+                infoSpeed = "extremely fast";
+            Console.WriteLine(infoSpeed);
+        }
+    }
+}
