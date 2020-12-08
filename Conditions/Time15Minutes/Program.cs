e71a4d3c752853cbdaa4bceff4c5aa8b470ef646
mila89 <doycheva_m@abv.bg>
2020-12-08 22:25:11 +0200

    Add files via upload

---
+++ Conditions/Time15Minutes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Time15Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hourIn = int.Parse(Console.ReadLine());
+            int minutesIn=int.Parse(Console.ReadLine());
+            minutesIn = minutesIn + 15;
+            int hourOut = 0;
+            int minutesOut = 0;
+            if (minutesIn > 59)
+            {
+                minutesOut = minutesIn - 60;
+                hourOut = hourIn + 1;
+                if (hourOut > 23)
+                    hourOut = 0;
+            }
+            else
+            {
+                minutesOut = minutesIn;
+                hourOut = hourIn;
+            }
+            if (minutesOut<10)
+                Console.WriteLine($"{hourOut}:0{minutesOut}");
+            else
+                Console.WriteLine($"{hourOut}:{minutesOut}");
+        }
+    }
+}
