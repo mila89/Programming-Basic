a82329a47760d1cf8d3acb7e1b7a5387aedabf31
mila89 <doycheva_m@abv.bg>
2020-12-08 22:21:41 +0200

    Add files via upload

---
+++ Conditions/Scholarship/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Scholarship
+{
+    class Program
+    {
+        static void Main()
+        {
+            double earnings = double.Parse(Console.ReadLine());
+            double averageProgress= double.Parse(Console.ReadLine());
+            double minSalary= double.Parse(Console.ReadLine());
+            double scholarshipScocial = 0;
+            double scholarshipProgress = 0;
+            if (averageProgress < 4.5)
+                Console.WriteLine("You cannot get a scholarship!");
+            else
+            {
+                Console.WriteLine("You cannot get a scholarship!dsds");
+                /* if (earnings > minSalary)
+                 {
+                     if (averageProgress >= 5.5)
+                     {
+                         scholarshipProgress = Math.Floor(5.5 * 25);
+                         Console.WriteLine($"You get a scholarship for excellent results {scholarshipProgress} BGN");
+                     }
+                 }
+                 else
+                 {
+                     if (averageProgress >= 5.5)
+                         scholarshipProgress = 5.5 * 25;
+                     else
+                         scholarshipScocial = minSalary * 35 / 100;
+                     if (scholarshipScocial > scholarshipProgress)
+                         Console.WriteLine($"You get a Social scholarship {Math.Floor(scholarshipScocial)} BGN");
+                     else
+                         Console.WriteLine($"You get a scholarship for excellent results {Math.Floor(scholarshipProgress)} BGN"); ;
+                 }*/
+            }
+        }
+    }
+}
