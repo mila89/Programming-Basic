c3d263e27cf7bf1b5232830e1012ef8b8838b986
mila89 <doycheva_m@abv.bg>
2020-12-08 22:25:49 +0200

    Add files via upload

---
+++ Conditions/WorldSwimmingRecord/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WorldSwimmingRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double bestRecord = double.Parse(Console.ReadLine()); //time in seconds
+            double distance = double.Parse(Console.ReadLine()); // distance in meters
+            double speedIvan=double.Parse(Console.ReadLine());
+            double speedWater = Math.Floor(distance / 15);
+            double timeIvan = distance * speedIvan + (speedWater * 12.5);
+            double result = (timeIvan - bestRecord);
+            if (timeIvan < bestRecord)
+                Console.WriteLine($"Yes, he succeeded! The new world record is {timeIvan,2:F} seconds.");
+            else
+                Console.WriteLine($"No, he failed! He was {result,2:F} seconds slower.");
+        }
+    }
+}
