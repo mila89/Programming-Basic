e84f7931ec0027e40b46b60eb0dcf94c940f217a
mila89 <doycheva_m@abv.bg>
2020-12-08 22:19:32 +0200

    Add files via upload

---
+++ Conditions/MetricConverter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MetricConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double inputNum = double.Parse(Console.ReadLine());
+            string mesureIn = Console.ReadLine();
+            string mesureOut = Console.ReadLine();
+            double result = 0;
+            if (mesureIn == "m")
+            {
+                if (mesureOut == "cm")
+                    result = inputNum * 100;
+                else
+                    result = inputNum * 1000;
+            }
+            else if (mesureIn == "cm")
+            {
+                if (mesureOut == "mm")
+                    result = inputNum * 10;
+                else
+                    result = inputNum / 100;
+            }
+            else if (mesureIn == "mm")
+            {
+                if (mesureOut == "cm")
+                    result = inputNum / 10;
+                else
+                    result = inputNum / 1000;
+            }
+
+            Console.WriteLine("{0:F3}", result);
+        }
+    }
+}
